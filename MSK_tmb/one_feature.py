ab879c9b27ef9601a00612695c3e136286d2c2e1
Jordan <omnesresnetwork@gmail.com>
2020-12-07 01:19:00 -0500

    sample info strat

--- MSK_tmb/one_feature.py
+++ MSK_tmb/one_feature.py
@@ -17,8 +17,9 @@ else:
     import sys
     sys.path.append(str(cwd))
 
-D, samples, maf, sample_df = pickle.load(open(cwd / 'MSK_tmb' / 'data' / 'all_cancers_data.pkl', 'rb'))
-panels = pickle.load(open(cwd / '..' / 'ATGC_paper' / 'files' / 'tcga_panel_table.pkl', 'rb'))
+
+D, samples, maf, sample_df = pickle.load(open(pathlib.Path('/home/janaya2/Desktop/ATGC2') / 'MSK_tmb' / 'data' / 'all_cancers_data.pkl', 'rb'))
+panels = pickle.load(open(pathlib.Path('/home/janaya2/Desktop/ATGC2') / '..' / 'ATGC_paper' / 'files' / 'tcga_panel_table.pkl', 'rb'))
 
 strand_emb_mat = np.concatenate([np.zeros(2)[np.newaxis, :], np.diag(np.ones(2))], axis=0)
 D['strand_emb'] = strand_emb_mat[D['strand']]
@@ -56,96 +57,92 @@ runs = 1
 initial_weights = []
 metrics = [RaggedModels.losses.QuantileLoss()]
 losses = [RaggedModels.losses.QuantileLoss()]
-callbacks = [tf.keras.callbacks.EarlyStopping(monitor='val_quantile_loss', min_delta=0.0001, patience=40, mode='min', restore_best_weights=True)]
 
 for i in range(runs):
-    # tile_encoder = InstanceModels.VariantSequence(6, 4, 2, [16, 16, 8, 8])
     tile_encoder = InstanceModels.PassThrough(shape=(1, ))
     sample_encoder = SampleModels.PassThrough(shape=(samples['histology'].shape[-1], ))
     mil = RaggedModels.MIL(mode='aggregation', instance_encoders=[tile_encoder.model], pooled_layers=[64], sample_layers=[64, 32], sample_encoders=[sample_encoder.model], output_dim=1, output_type='quantiles')
+    # mil.model.trainable = False
     mil.model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001), loss=losses, metrics=metrics)
     initial_weights.append(mil.model.get_weights())
 
-weights = []
 ##stratified K fold for test
 for idx_train, idx_test in StratifiedKFold(n_splits=8, random_state=0, shuffle=True).split(y_strat, y_strat):
+    pass
 
-    idx_train, idx_valid = [idx_train[idx] for idx in list(StratifiedShuffleSplit(n_splits=1, test_size=1500, random_state=0).split(np.zeros_like(y_strat)[idx_train], y_strat[idx_train]))[0]]
-
-    ds_train = tf.data.Dataset.from_tensor_slices((idx_train, y_label[idx_train], y_strat[idx_train]))
-    ds_train = ds_train.apply(DatasetsUtils.Apply.StratifiedMinibatch(batch_size=512, ds_size=len(idx_train)))
-    # x_loader = DatasetsUtils.Map.LoadBatchIndex(loaders=[DatasetsUtils.Loaders.FromNumpy(five_p, tf.int32),
-    #                                                      DatasetsUtils.Loaders.FromNumpy(three_p, tf.int32),
-    #                                                      DatasetsUtils.Loaders.FromNumpy(ref, tf.int32),
-    #                                                      DatasetsUtils.Loaders.FromNumpy(alt, tf.int32),
-    #                                                      DatasetsUtils.Loaders.FromNumpy(strand, tf.float32)])
-
-    x_loader = DatasetsUtils.Map.LoadBatchIndex(loaders=[DatasetsUtils.Loaders.FromNumpy(ones, tf.float32),
-                                                         DatasetsUtils.Loaders.FromNumpy(np.array([[i] for i in samples['histology']], dtype='object'), tf.float32)])
-
-    # ds_train = ds_train.map(lambda x, y: (x_loader(x, to_ragged=[True, True, True, True, True]), y))
-    ds_train = ds_train.map(lambda x, y: (x_loader(x, to_ragged=[True, False]), y))
-
-    ds_valid = tf.data.Dataset.from_tensor_slices((idx_valid, y_label[idx_valid]))
-    ds_valid = ds_valid.batch(len(idx_valid), drop_remainder=False)
-    # ds_valid = ds_valid.map(lambda x, y: (x_loader(x, to_ragged=[True, True, True, True, True]), y))
-    ds_valid = ds_valid.map(lambda x, y: (x_loader(x, to_ragged=[True, False]), y))
-    eval = 100
-    for initial_weight in initial_weights:
-        mil.model.set_weights(initial_weight)
-        mil.model.fit(ds_train, validation_data=ds_valid,
-                      epochs=10000,
-                      callbacks=callbacks)
-        run_eval = mil.model.evaluate(ds_valid)[1]
-        if run_eval < eval:
-            eval = run_eval
-            run_weights = mil.model.get_weights()
-
-    weights.append(run_weights)
+idx_train, idx_valid = [idx_train[idx] for idx in list(StratifiedShuffleSplit(n_splits=1, test_size=1500, random_state=0).split(np.zeros_like(y_strat)[idx_train], y_strat[idx_train]))[0]]
 
 
-# with open('tmb_weights.pkl', 'wb') as f:
-#     pickle.dump(weights, f)
+from sklearn.model_selection import RepeatedStratifiedKFold
+
+class Apply:
+    class StratifiedMinibatch:
+        def __init__(self, batch_size, ds_size):
+            self.batch_size, self.ds_size = batch_size, ds_size
+            # max number of splits
+            self.n_splits = self.ds_size // self.batch_size
+            # stratified "mini-batch" via k-fold
+            self.batcher = RepeatedStratifiedKFold(n_splits=self.n_splits, n_repeats=10000)
+
+        def __call__(self, ds_input: tf.data.Dataset):
+            def generator():
+                idx, *samples, y_true, y_strat = list(map(tf.stack, list(map(list, zip(*list(ds_input.take(self.n_splits * self.batch_size)))))))
+                for _, idx_batch in self.batcher.split(y_strat, y_strat):
+                    samples_batched = [tf.gather(sample, idx_batch, axis=0) for sample in samples]
+                    yield(tuple([tf.gather(idx, idx_batch, axis=0)] + samples_batched + [tf.gather(y_true, idx_batch, axis=0)]))
+
+            return tf.data.Dataset.from_generator(generator,
+                                                  output_types=tuple([i.dtype for i in ds_input.element_spec[: -1]]),
+                                                  )
 
 
 
-##test eval
-test_idx = []
-predictions = []
+
+ds_train = tf.data.Dataset.from_tensor_slices((idx_train, samples['histology'][idx_train], y_label[idx_train], y_strat[idx_train]))
+# ds_train.shuffle(buffer_size=len(idx_train), reshuffle_each_iteration=True)
+ds_train = ds_train.apply(Apply.StratifiedMinibatch(batch_size=512, ds_size=len(idx_train)))
+x_loader = DatasetsUtils.Map.LoadBatchIndex(loaders=[DatasetsUtils.Loaders.FromNumpy(ones, tf.float32)])
+ds_train = ds_train.repeat().map(lambda *args: (((x_loader(args[0], to_ragged=[True]), ) + args[1: -1]), args[-1]))
 
 
-for index, (idx_train, idx_test) in enumerate(StratifiedKFold(n_splits=8, random_state=0, shuffle=True).split(y_strat, y_strat)):
-    mil.model.set_weights(weights[index])
-    ds_test = tf.data.Dataset.from_tensor_slices((idx_test, y_label[idx_test]))
-    ds_test = ds_test.batch(len(idx_test), drop_remainder=False)
-    ds_test = ds_test.map(lambda x, y: (x_loader(x, to_ragged=[True, False]), y))
-    predictions.append(mil.model.predict(ds_test))
-    test_idx.append(idx_test)
-from sklearn.metrics import r2_score
 
-#mse
-print(round(np.mean((y_label[:, 0][np.concatenate(test_idx)] - np.concatenate(predictions)[:, 1])**2), 4))
-#mae
-print(round(np.mean(np.absolute(y_label[:, 0][np.concatenate(test_idx)] - np.concatenate(predictions)[:, 1])), 4))
-#r2
-print(round(r2_score(np.concatenate(predictions)[:, 1], y_label[:, 0][np.concatenate(test_idx)]), 4))
+ds_valid = tf.data.Dataset.from_tensor_slices((idx_valid, samples['histology'][idx_valid], y_label[idx_valid], y_strat[idx_valid]))
+# ds_valid = ds_valid.apply(Apply.StratifiedMinibatch(batch_size=512, ds_size=len(idx_valid)))
+ds_valid = ds_valid.batch(len(idx_valid), drop_remainder=False)
+ds_valid = ds_valid.map(lambda *args: (((x_loader(args[0], to_ragged=[True]), ) + args[1: -1]), args[-1]))
+
+
+
+callbacks = [tf.keras.callbacks.EarlyStopping(monitor='val_quantile_loss', min_delta=0.0001, patience=40, mode='min', restore_best_weights=True)]
+
+for initial_weight in initial_weights:
+    mil.model.set_weights(initial_weight)
+    mil.model.fit(ds_train,
+                  steps_per_epoch=20,
+                  # validation_data=ds_valid,
+                  epochs=1000,
+                  # callbacks=callbacks
+                  )
+
+
+
+
 
 #
+# ds_all = tf.data.Dataset.from_tensor_slices((np.arange(len(y_label)), samples['histology'], y_label))
+# ds_all = ds_all.batch(len(y_label))
+# ds_all = ds_all.map(lambda *args: (((x_loader(args[0], to_ragged=[True]), ) + args[1: -1]), args[-1]))
+
+# predictions = mil.model.predict(ds_train)
+
 #
-# import pandas as pd
-# ##counting has to be nonsyn to nonsyn
-# panel_counts = maf[['Tumor_Sample_Barcode']].groupby('Tumor_Sample_Barcode').apply(lambda x: pd.Series([len(x)], index=['panel_all_counts']))
-# sample_df = pd.merge(sample_df, panel_counts, how='left', on='Tumor_Sample_Barcode')
-# sample_df.fillna({'panel_all_counts': 0}, inplace=True)
-#
-# results={}
-# results['counting'] = np.log(sample_df['panel_all_counts'].values[np.concatenate(test_idx)] / (panels.loc[panels['Panel'] == 'MSK-IMPACT468']['cds'].values[0]/1e6) + 1)
-#
-#
-# ##counting stats
-# #mse
-# print(round(np.mean((y_label[:, 0][np.concatenate(test_idx)] - results['counting'])**2), 4))
+# for cancer in np.unique(sample_df['type'].values):
+#     mask = sample_df['type'] == cancer
+#     print(cancer, round(np.mean((y_label[:,0][mask] - predictions[:, 1][mask])**2), 4), sum(mask))
+
+
 # #mae
-# print(round(np.mean(np.absolute((y_label[:, 0][np.concatenate(test_idx)] - results['counting']))), 4))
+# print(round(np.mean(np.absolute(y_label[:, 0][np.concatenate(test_idx)] - np.concatenate(predictions)[:, 1])), 4))
 # #r2
-# print(round(r2_score(results['counting'], y_label[np.concatenate(test_idx)][:, 0]), 4))
+# print(round(r2_score(np.concatenate(predictions)[:, 1], y_label[:, 0][np.concatenate(test_idx)]), 4))
+
