7036b51148ed068f0c8e4f96f6762a9d0ce66876
Jordan <omnesresnetwork@gmail.com>
2021-10-19 16:34:40 -0400

    code update

--- figures/controls/data_preprocessing.py
+++ figures/controls/data_preprocessing.py
@@ -6,27 +6,17 @@ from Bio.Seq import Seq
 import pyranges as pr
 import pathlib
 path = pathlib.Path.cwd()
-if path.stem == 'ATGC2':
+if path.stem == 'ATGC':
     cwd = path
 else:
-    cwd = list(path.parents)[::-1][path.parts.index('ATGC2')]
+    cwd = list(path.parents)[::-1][path.parts.index('ATGC')]
 
 
 tcga_maf = pickle.load(open(cwd / 'files' / 'tcga_maf_table.pkl', 'rb'))
+tcga_maf = tcga_maf.loc[tcga_maf['CDS'] > 0]
 samples = pickle.load(open(cwd / 'files' / 'tcga_sample_table.pkl', 'rb'))
 ##there's a bad sample with nans
 tcga_maf.dropna(inplace=True)
-tcga_maf.reset_index(inplace=True, drop=True)
-tcga_maf = pd.merge(tcga_maf, samples.Tumor_Sample_Barcode.reset_index(), how='left', on='Tumor_Sample_Barcode')
-samples_idx = tcga_maf['index'].values
-
-
-# 5p, 3p, ref, alt
-nucleotide_mapping = {'-': 0, 'N': 0, 'A': 1, 'T': 2, 'C': 3, 'G': 4}
-seqs_5p = np.stack(tcga_maf.five_p.apply(lambda x: np.array([nucleotide_mapping[i] for i in x])).values, axis=0)
-seqs_3p = np.stack(tcga_maf.three_p.apply(lambda x: np.array([nucleotide_mapping[i] for i in x])).values, axis=0)
-seqs_ref = np.stack(tcga_maf.Ref.apply(lambda x: np.array([nucleotide_mapping[i] for i in x])).values, axis=0)
-seqs_alt = np.stack(tcga_maf.Alt.apply(lambda x: np.array([nucleotide_mapping[i] for i in x])).values, axis=0)
 
 ##map genomic coordinates to the exome (takes some time)
 
@@ -47,14 +37,46 @@ gff_dfs = gff_cds_pr.dfs
 for df in gff_dfs:
     gff_dfs[df]['length'] = gff_dfs[df]['End'] - gff_dfs[df]['Start']
 
+
 cum_pos = []
+interval_pos = []
+mask = []
 for row in tcga_maf.itertuples():
     cum_pos.append(gff_dfs[row.Chromosome][gff_dfs[row.Chromosome]['Start'] < row.Start_Position]['length'].sum())
+    try:
+        interval_pos.append(row.Start_Position - gff_dfs[row.Chromosome].loc[(gff_dfs[row.Chromosome]['Start'] <= row.Start_Position) & (gff_dfs[row.Chromosome]['End'] >= row.Start_Position)]['Start'].values[0])
+        mask.append(True)
+    except:
+        try:
+            interval_pos.append(row.Start_Position - gff_dfs[row.Chromosome].loc[(gff_dfs[row.Chromosome]['Start'] <= row.End_Position) & (gff_dfs[row.Chromosome]['End'] >= row.End_Position)]['Start'].values[0])
+            mask.append(True)
+        except:
+            mask.append(False)
+
+cum_pos = np.array(cum_pos)[mask]
+tcga_maf = tcga_maf[mask]
 
 chromosome_cds_sizes = {df: gff_dfs[df]['length'].sum() for df in gff_dfs}
+accumulated_sizes = {}
+for chromosome_0 in gff_dfs:
+    size = 0
+    for chromosome_1 in gff_dfs:
+        if chromosome_0 < chromosome_1:
+            size += chromosome_cds_sizes[chromosome_1]
+    accumulated_sizes[chromosome_0] = size
 
-gen_pos = np.array(cum_pos) / [chromosome_cds_sizes[i] for i in tcga_maf.Chromosome.values]
+cum_pos = cum_pos + np.array(interval_pos) + np.array([accumulated_sizes[i] for i in tcga_maf.Chromosome.values])
 
+tcga_maf.reset_index(inplace=True, drop=True)
+tcga_maf = pd.merge(tcga_maf, samples.Tumor_Sample_Barcode.reset_index(), how='left', on='Tumor_Sample_Barcode')
+samples_idx = tcga_maf['index'].values
+
+# 5p, 3p, ref, alt
+nucleotide_mapping = {'-': 0, 'N': 0, 'A': 1, 'T': 2, 'C': 3, 'G': 4}
+seqs_5p = np.stack(tcga_maf.five_p.apply(lambda x: np.array([nucleotide_mapping[i] for i in x])).values, axis=0)
+seqs_3p = np.stack(tcga_maf.three_p.apply(lambda x: np.array([nucleotide_mapping[i] for i in x])).values, axis=0)
+seqs_ref = np.stack(tcga_maf.Ref.apply(lambda x: np.array([nucleotide_mapping[i] for i in x])).values, axis=0)
+seqs_alt = np.stack(tcga_maf.Alt.apply(lambda x: np.array([nucleotide_mapping[i] for i in x])).values, axis=0)
 
 def get_cds(cds):
     if '-' in cds:
@@ -84,11 +106,12 @@ instances = {'sample_idx': samples_idx,
              'seq_ref': seqs_ref,
              'seq_alt': seqs_alt,
              'chr': gen_chr,
-             'pos_float': gen_pos,
+             'cum_pos': cum_pos,
              'cds': cds,
 }
 
 variant_encoding = np.array([0, 2, 1, 4, 3])
+
 instances['seq_5p'] = np.stack([instances['seq_5p'], variant_encoding[instances['seq_3p'][:, ::-1]]], axis=2)
 instances['seq_3p'] = np.stack([instances['seq_3p'], variant_encoding[instances['seq_5p'][:, :, 0][:, ::-1]]], axis=2)
 t = instances['seq_ref'].copy()
